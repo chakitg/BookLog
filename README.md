76f2de898747dd602605fadf2c6bb6a1d346dc7b
Chakit Gupta <chakitg1@gmail.com>
2025-03-05 01:16:03 +0530

    completed project

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
 # BookLog
+
+![Project Thumbnail](https://i.ibb.co/yhzRCG5/amazing-book-tracker-preview.jpg)
+
+## Description
+
+**The Amazing BookLog** is a web-based application created wiht the goal to provide users with a means to manage their book collections.
+
+End users can record the books they've previously read, books they want to read; as well as the books they're currently reading.
+
+## Functionality
+
+Users are able to add a book to the system.
+
+Users should be able to place books on 3 separate lists:
+
+1. Books they've already read
+2. Books they want to read
+3. Books they are currently reading.
+
+Finally, users should be able to easily navigate through these separate lists.
+
+## Getting Started
+
+### Prerequisites
+
+[NodeJS](https://nodejs.org/en/) and [NPM](https://www.npmjs.com/) should be installed.
+
+### Clone the repository
+
+```sh
+git clone https://github.com/chakitg/BookLog.git
+```
+
+### Install dependencies
+
+```sh
+cd BookLog
+npm install
+```
+
+### Start development
+
+To start the development server for the React version:
+
+```sh
+npm start
+```
+
+The frontend app will be running on `localhost:3000`.
+
+# Acknowledgements
+
+## Favicons
+
+React App Favicon: Icon by Freepik on flaticon.com, and made with [realfavicongenerator.net](https://realfavicongenerator.net/).
---
+++ src/components/SignUpPage/SignUpPage.jsx
@@ -0,0 +1,94 @@
+import React from "react";
+import { useState } from "react";
+import SignUpService from "../../services/signUp.service";
+import { AlteredTextField } from "./AlteredTextField/AlteredTextField";
+import { formFields } from "./SignUpFormFields";
+
+const SignUpPage = ({ history }) =>{
+
+  const [data, setData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    confirmPassword: ''
+  })
+  const [errors, setErrors] = useState({})
+
+  const handleSubmit = async (e) =>{
+    e.preventDefault()
+    const {name, email, password, confirmPassword} = data
+
+    if(password !== confirmPassword){
+      // setErrors({...errors, ["confirmPassword"]:"passwords dont match"})
+      setErrors({['confirmPassword']: 'passwords dont match'})
+    }else{
+      const {body, error} = await SignUpService.request(JSON.stringify({name, email, password}))
+
+      if(error){
+        setErrors({...body.details})
+        return
+      }
+
+      localStorage.setItem("token", body.token)
+      history.push("/")
+    }
+  }
+
+  const handleChange = (e) =>{
+    const {name, value} = e.target
+    setData({...data, [name]:value})
+  }
+
+  const showPassword = () =>{
+    document.getElementById('password').type = document.getElementById('password').type === 'text'?'password':'text'
+    document.getElementById('confirmPassword').type = document.getElementById('confirmPassword').type === 'text'?'password':'text'
+  }
+
+
+  return(
+    <React.Fragment>
+      <form className='form-container'>
+        <h2>Sign Up</h2>
+        <input 
+        type="text" 
+        name='name' 
+        id='name' 
+        value={data.name}
+        placeholder="name"
+        onChange={handleChange} 
+      />
+      <input 
+        type="email" 
+        name='email' 
+        id='email'
+        value={data.email}
+        placeholder="email"
+        onChange={handleChange} 
+      />
+      <input 
+        type="password" 
+        name='password'
+        id='password'
+        value={data.password}
+        placeholder="password"
+        onChange={handleChange} 
+      />
+      <input 
+        type="password"
+        name='confirmPassword'
+        id='confirmPassword'
+        value={data.confirmPassword}
+        placeholder="Confirm Password"
+        onChange={handleChange} 
+      />
+      </form>
+      <div style={{ position: "relative", left: "29%", width: "100%" }}>
+        <input type="checkbox" onClick={showPassword} style={{ width: "2%" }} />
+        <span style={{ color: "white", fontSize: "0.8rem" }}>Show password</span>
+      </div>
+      <button style={{ position: "relative", left: "43%" }} onClick={handleSubmit}>Register</button>
+    </React.Fragment>
+  )
+}
+
+export default SignUpPage
